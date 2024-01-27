be2cd454754cf5e1999c15ae5068993afb2c04fb
Sergey Tselovalnikov <serg310583@mail.ru>
2024-01-27 16:47:24 +0700

    SEO(добавлен тэг main), кн. вверх (замена JS на CSS, плавный скролл) pixelperfect Promo

--- src/js/index.js
+++ src/js/index.js
@@ -14,27 +14,27 @@ document
     }
   });
 // ФУНКЦИОНАЛ ПРОКРУТКИ ВВЕРХ
-const goTopBtns = document.querySelectorAll('.go-top');
-goTopBtns.forEach((btn) => {
-  btn.addEventListener('click', goTop);
-});
+// const goTopBtns = document.querySelectorAll('.go-top');
+// goTopBtns.forEach((btn) => {
+//   btn.addEventListener('click', goTop);
+// });
 
-function trackScroll() {
-  const scrolled = window.scrollY;
-  const coords = document.documentElement.clientHeight;
-  if (scrolled > coords) {
-    goTopBtn.classList.add('go-top--show');
-  } else {
-    goTopBtn.classList.remove('go-top--show');
-  }
-}
+// function trackScroll() {
+//   const scrolled = window.scrollY;
+//   const coords = document.documentElement.clientHeight;
+//   if (scrolled > coords) {
+//     goTopBtn.classList.add('go-top--show');
+//   } else {
+//     goTopBtn.classList.remove('go-top--show');
+//   }
+// }
 
-function goTop() {
-  if (window.scrollY > 0) {
-    window.scrollBy(0, -55);
-    setTimeout(goTop, 0);
-  }
-}
+// function goTop() {
+//   if (window.scrollY > 0) {
+//     window.scrollBy(0, -55);
+//     setTimeout(goTop, 0);
+//   }
+// }
 
 // ФУНКЦИОНАЛ КНОПОК ПЛЮС И МИНУС ДЛЯ РАЗРЕШЕНИЯ 375PX
 // Находим все блоки с классом "button-container"
